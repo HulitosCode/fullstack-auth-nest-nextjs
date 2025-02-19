01e2ce23123c322a66543d92d2400a0ad3e9c814
HulitosCode <heltonmatimbe8@gmail.com>
2025-02-19 16:42:45 +0200

    fix: remove enableCors

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ?? 3001);
   // Habilitar CORS para todos os domínios ou um domínio específico
-  app.enableCors({
-    origin: 'https://fullstack-auth-nest-nextjs-1.onrender.com/', // Altere para o URL do seu frontend
-    methods: 'GET,POST,PATCH,PUT,DELETE',
-    // allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: 'https://fullstack-auth-nest-nextjs-1.onrender.com/', // Altere para o URL do seu frontend
+  //   methods: 'GET,POST,PATCH,PUT,DELETE',
+  //   // allowedHeaders: 'Content-Type, Authorization',
+  //   credentials: true,
+  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
